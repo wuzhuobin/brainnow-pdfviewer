333be2d5d2426b274e09d6119bd54545067686fb
gundam <gundam@lenovo>
2019-05-08 23:36:44 +0800

    PDF functions are added. 1. adding some test resources. 2. using grid with 2 row only.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './asset/logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
 import PageHeader from 'antd/lib/page-header'
 import Button from 'antd/lib/button';
+import Col from 'antd/lib/col';
+import Row from 'antd/lib/row'
 import BrainnowIcon from './asset/brainnow-icon.svg';
+import PDFTest from './asset/C2-Glossary.pdf';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <PageHeader title={<img src={BrainnowIcon} alt="BrainnowIcon" className="logoImg"></img>}
-          extra={<Button className="lanBtn">Chinese/中文</Button>}
+          extra={<div className="lanBtn"><Button >Chinese/中文</Button></div>}
         >
         </PageHeader>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
+        <Row type="flex">
+          <Col span={12}><embed className="PDFViewer" src={PDFTest} type="application/pdf"></embed></Col>
+          <Col span={12}><img className="JPGViewer" src={logo} alt="logo"></img></Col>
+        </Row>
       </div>
     );
   }
