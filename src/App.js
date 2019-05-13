2e3151535a0cf2b20dc80f103a8e6a34ff4e8eb2
wuzhuobin <zhuobin.wu@philips.com>
2019-05-13 01:29:12 -0700

    BrainnowPDFViewer added, test pdf and jpg updated. 1. create a react component for the viewer functions.

--- src/App.js
+++ src/App.js
@@ -1,52 +1,12 @@
 import React from 'react';
-import test0 from './asset/test0.jpg';
-import test1 from './asset/test1.jpg';
-import './App.css';
-import 'antd/dist/antd.css';
-import PageHeader from 'antd/lib/page-header'
-import BrainnowIcon from './asset/brainnow-icon.svg';
-import PDFTest from './asset/C2-Glossary.pdf';
-// import { PageHeader } from 'antd';
-import { Button } from 'antd';
-import { Table } from 'antd';
-import { Row } from 'antd';
-import { Col } from 'antd';
+import test0 from './asset/512x512.jpg';
+import test1 from './asset/1024x1024.jpg';
+import pdf_en_us from './asset/I am pdf en-us.pdf';
+import pdf_zh_cn from './asset/I am pdf zh-cn.pdf';
+import BrainnowPDFViewer from './BrainnowPDFViewer';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      index: 0
-    }
-    this.imgs = [test0, test1];
-  }
-  render() {
-    const columns = [{
-      title: 'img',
-      dataIndex: 'img'
-    }];
-    const dataSource = this.imgs.map((value, index) => {
-      return {
-        key: index,
-        img: <img src={value} alt="logo" onContextMenu={e => e.preventDefault()}></img>
-      }
-    });
-       
-    return (
-      <div className="App">
-        <PageHeader title={<img src={BrainnowIcon}  alt="BrainnowIcon" className="logoImg"></img>}
-          extra={<div className="lanBtn"><Button >Chinese/中文</Button></div>}
-        >
-        </PageHeader>
-        <Row type="flex">
-          <Col span={12}><embed className="PDFViewer" src={PDFTest} type="application/pdf"></embed></Col>
-          <Col span={12}>
-            <Table className="JPGViewer" showHeader={false} pagination={false} columns={columns} dataSource={dataSource} scroll={{y: '91vh'}}></Table>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default function App() {
+  return (
+    <BrainnowPDFViewer pdf_en_us={pdf_en_us} pdf_zh_cn={pdf_zh_cn} imgs={[test0, test1]}></BrainnowPDFViewer>
+  );
+};
